5919ffed24201ef11b2bef8f3dacf853d6b1f91b
wahidari <wahiid.ari@gmail.com>
2023-02-18 19:09:42 +0700

    add OG Image

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withNextra = require('nextra')({
 
 const nextConfig = {
   env: {
-    API_URL: "http://localhost:3000",
+    API_URL: "http://localhost:3001",
     // API_URL: "https://my-musicc.vercel.app",
   }
 };
